c73bb2ce7676d5d3b53c94fd8b93d9a175174c3c
kangsg <kangsg26@naver.com>
2019-08-05 17:12:22 +0900

    1. login 프로세스 개발  - jsp to jsp --> servlet to jsp 2. bootstrap 적용  - login.jsp  - main.jsp 3. 웹 어플리케이션 scope  - application  - session(*)  - request(*)  - pageContext  -- attribute : 공유할 데이터  -- servlet에서 처리한 데이터를 jsp에서 사용할 수 있게끔 공유

--- src/main/java/kr/or/ddit/user/model/UserVo.java
+++ src/main/java/kr/or/ddit/user/model/UserVo.java
@@ -1,12 +1,18 @@
 package kr.or.ddit.user.model;
 
 public class UserVo {
+	private String userId;			// 사용자 아이디
+	private String pass;			// 사용자 패스워드 
 	private String userName;		// 사용자 이름
 
 	public String getUserName() {
 		return userName;
 	}
-
+	
+	public UserVo() {
+		
+	}
+	
 	public UserVo(String userName) {
 		super();
 		this.userName = userName;
@@ -16,8 +22,24 @@ public class UserVo {
 		this.userName = userName;
 	}
 
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
 	@Override
 	public String toString() {
-		return "UserVo [userName=" + userName + "]";
+		return "UserVo [userId=" + userId + ", pass=" + pass + ", userName=" + userName + "]";
 	}
 }
