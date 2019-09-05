9008cfc345824f702a871dfa37bd7d4503038125
kangsg <kangsg26@naver.com>
2019-09-05 13:42:42 +0900

    requestCounterFilter 기능변경  . key : uri --> uri + method  . RequestSortVO

--- src/main/java/kr/or/ddit/filter/RequestSortVO.java
+++ src/main/java/kr/or/ddit/filter/RequestSortVO.java
@@ -39,5 +39,39 @@ public class RequestSortVO {
 		return "RequestSortVO [uri=" + uri + ", method=" + method + ", uriCount=" + uriCount + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((method == null) ? 0 : method.hashCode());
+		result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+		result = prime * result + uriCount;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RequestSortVO other = (RequestSortVO) obj;
+		if (method == null) {
+			if (other.method != null)
+				return false;
+		} else if (!method.equals(other.method))
+			return false;
+		if (uri == null) {
+			if (other.uri != null)
+				return false;
+		} else if (!uri.equals(other.uri))
+			return false;
+		if (uriCount != other.uriCount)
+			return false;
+		return true;
+	}
+
 	
 }
